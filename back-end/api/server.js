0b2f3095f88ee6dddec35429b343f4abcb843cdf
unknown <phelipedsilveira@gmail.com>
2025-05-12 01:50:05 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -16,22 +16,22 @@ const PORT = 3000;
 
 app.use(cors());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Só vamos trabalhar com os endpoints '/artists' '/songs'");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
 });
 
-app.use(express.static(path.join(__dirname, "/.././front-end/dist")));
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
 
 app.get("#", async (request, response) => {
-  response.sendFile(path.join(__dirname, "/../front-end/dist/index.html"));
+  response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
